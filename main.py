b574259b0b992193697434db2860f29ac7ee67e6
PavelPos <79365094+AlexandrGroz@users.noreply.github.com>
2025-09-11 04:18:20 +0500

    left only 3 columns

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas as pd
 from catboost import datasets
 
 #Task 1
+"""
 (train_df, test_df) = datasets.titanic()
 df = pd.concat([train_df, test_df], ignore_index=True)
 
+df.to_csv("datasets/titanic.csv", index=False)
+print("Saved: datasets/titanic.csv", df.shape)
+"""
+
+#Task 2
+df = pd.read_csv("datasets/titanic.csv", usecols=["Pclass", "Sex", "Age"])
 df.to_csv("datasets/titanic.csv", index=False)
 print("Saved: datasets/titanic.csv", df.shape)
